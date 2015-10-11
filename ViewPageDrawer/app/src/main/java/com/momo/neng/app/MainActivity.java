bb731a56526facc3521f5c0f284e1e244c0e37a6
lineng <383356258@qq.com>
2015-10-11 12:48:55 +0800

    String Tag

--- ViewPageDrawer/app/src/main/java/com/momo/neng/app/MainActivity.java
+++ ViewPageDrawer/app/src/main/java/com/momo/neng/app/MainActivity.java
@@ -6,7 +6,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
-
+	private String Tag = this.getClass.getName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
